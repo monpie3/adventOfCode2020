0dfa071e4d5ff7efb5b88d8e0a7090200f550c4f
Monika <dotyk_zla@o2.pl>
2022-02-22 19:22:38 +0100

    Day 8

--- Day_8/task8b.py
+++ Day_8/task8b.py
@@ -1,18 +1,24 @@
+import copy
+
+
 def get_instructions(filename):
     with open(filename) as file:
         instructions = file.read().splitlines()
-        instructions = [tuple(instruction.split()) for instruction in instructions]
+        instructions = [instruction.split() for instruction in instructions]
     return instructions
 
-if __name__ == "__main__":
-    instructions = get_instructions("puzzle_input.txt")
-    print(instructions)
 
-    diffrent_ind = True
+def accumulator(instructions):
+    print("accumulator", instructions)
+    stay_in_infinite_loop = True
     list_of_ind = []
     ind = 0
     acc = 0
-    while diffrent_ind:
+
+    while stay_in_infinite_loop:
+        if len(instructions) - 1 < ind:
+            return True, acc
+
         op, argument = instructions[ind]
         argument = int(argument)
         print("Operation:", op, argument, ". Acc before:", acc, ". i before:", ind)
@@ -31,8 +37,36 @@ if __name__ == "__main__":
 
         if ind in list_of_ind:
             print("Program would run an instruction a second time")
-            diffrent_ind = False
+            stay_in_infinite_loop = False
+            return False, "program didn't terminated"
 
         list_of_ind.append(ind)
 
-    print("acc", acc)
+
+def swap_op(instructions, step):
+    """swaps op from nop to jmp or jmp to nop"""
+    instr = copy.deepcopy(instructions)
+    ops, _ = zip(*instr)
+    for i in range(step, len(ops)):
+        if ops[i] == "jmp":
+            instr[i][0] = "nop"
+            return instr, i + 1
+        if ops[i] == "nop":
+            instr[i][0] = "jmp"
+            return instr, i + 1
+
+
+def terminate_program_by_swap(instructions):
+    terminated = False
+    step = 0
+    while not terminated:
+        n_instructions, step = swap_op(instructions, step)
+        terminated, acc = accumulator(n_instructions)
+        print("acc", acc)
+    return acc
+
+
+if __name__ == "__main__":
+    instructions = get_instructions("puzzle_input.txt")
+    acc = terminate_program_by_swap(instructions)
+    print("Puzzle answer:", acc)
